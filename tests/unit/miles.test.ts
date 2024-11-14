116bb3e75215b6e4967ba27122a62d2a4287f7df
Matheus Henrique <matthpmoreira@gmail.com>
2024-11-14 09:49:55 -0300

    Test miles generation

--- tests/unit/miles.test.ts
+++ tests/unit/miles.test.ts
@@ -32,3 +32,35 @@ describe("Tests for retrieving a Miles schema", () => {
     });
   });
 });
+
+describe("Tests for creating a Miles schema", () => {
+  it("should generate a Miles schema when given a Trip schema", async () => {
+    const trip = TripFactory();
+    const miles = faker.number.int();
+
+    jest.spyOn(MilesRepository, "findMiles").mockResolvedValueOnce(null);
+    jest.spyOn(MilesRepository, "saveMiles").mockResolvedValueOnce(null);
+    jest.spyOn(MilesCalculator, "calculateMiles").mockReturnValueOnce(miles);
+
+    const result = await generateMilesForTrip(trip);
+
+    expect(result).toEqual(miles);
+    expect(MilesRepository.findMiles).toHaveBeenCalledTimes(1);
+    expect(MilesRepository.saveMiles).toHaveBeenCalledTimes(1);
+    expect(MilesCalculator.calculateMiles).toHaveBeenCalledTimes(1);
+  });
+
+  it("should throw 'conflict' if code is already in database", async () => {
+    const trip = TripFactory();
+    const miles = MilesFactory();
+
+    jest.spyOn(MilesRepository, "findMiles").mockResolvedValueOnce(miles);
+
+    const promise = generateMilesForTrip(trip);
+    expect(MilesRepository.findMiles).toHaveBeenCalledTimes(1);
+    expect(promise).rejects.toEqual({
+      type: "conflict",
+      message: `Miles already registered for code ${trip.code}`,
+    });
+  });
+});
